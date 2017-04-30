eecbd90889488d9d9d13f06dcec07ca5ddc4bb70
Rui Filipe de Sousa Martins <ruimartinsptl@gmail.com>
2017-04-30 23:02:42 +0100

    UP

--- actuators_motor_drivers.py
+++ actuators_motor_drivers.py
@@ -1,16 +1,23 @@
 import constants as co
 
 class DRV8825:
-    def __init__(self, GPIO, stepper, pin_step, pin_dir, direction_inverted=False, stepps_by_meter=None):
+    def __init__(self, GPIO, stepper, pin_step, pin_dir, name="", direction_inverted=False, stepps_by_meter=None):
         self._GPIO = GPIO
         self._stepper = stepper
         self._pin_step = pin_step
         self._pin_dir = pin_dir
+        self._name = name
         self._direction_inverted = direction_inverted
         self._stepps_by_meter = stepps_by_meter
-
+        print "setup pin_step", pin_step, " as OUT"
         GPIO.setup(pin_step, self._GPIO.OUT)
+        print "setup pin_dir", pin_dir, " as OUT"
         GPIO.setup(pin_dir, self._GPIO.OUT)
+
+    def cleanup_gpio_pins(self):
+        for pin in (self._pin_step, self._pin_dir):
+            print "cleaning pin", pin, "from '", self._name, "'."
+            self._GPIO.cleanup(pin)
 
     def move_stepps(self, direction_front, stepps):
         if self._direction_inverted:
--- main.py
+++ main.py
@@ -12,3 +12,4 @@ GPIO.setmode(GPIO.BCM)
 motor_left = DRV8825(GPIO, STEPPER_42BYGHW811(stepper_type=co.MOTOR_FULL_STEPS), pin_step=2, pin_dir=3)
 motor_right = DRV8825(GPIO, STEPPER_42BYGHW811(stepper_type=co.MOTOR_HALF_STEPS), pin_step=4, pin_dir=17)
 
+motor_left.cleanup_gpio_pins()
